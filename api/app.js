f17b5a6e812d705418c593e5e68f755729ad69a0
Xettman (Desktop) <xettman777@gmail.com>
2025-09-18 19:04:59 +1000

    Added api route mount to server

--- api/app.js
+++ api/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const routes = require('./routes');
 const PORT = 3000;
 
 // Serve static files from the 'frontend' folder
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
+// Mount API routes
+app.use('/api', routes);
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
