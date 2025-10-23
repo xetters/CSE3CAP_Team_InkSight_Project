2e40cc237bb045aeb9a51a3ce09812310ae1b143
Xettman (Desktop) <xettman777@gmail.com>
2025-10-24 03:49:38 +1100

    ai art glass

--- api/app.js
+++ api/app.js
@@ -7,6 +7,9 @@ const PORT = 3000;
 // Serve static files from the 'frontend' folder
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
+// Serve static files from the 'media' folder
+app.use('/media', express.static(path.join(__dirname, '..', 'media')));
+
 // Mount API routes
 app.use('/api', routes);
 
